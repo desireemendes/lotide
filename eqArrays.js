9ff166fcd7b89fd4e9f620d47d7d9d621a4bf114
Desiree Mendes <desireefrost@live.ca>
2021-09-15 13:27:45 -0400

    initial

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (Array.isArray(actual[i]) && Array.isArray(expected[i])) {
+        if (!eqArrays(actual[i], expected[i])) {
+          return false;
+        }
+      } else if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};

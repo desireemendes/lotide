dc1968d00a3f6d9882d3c30cb2cfdcdab02db572
Desiree Mendes <desireefrost@live.ca>
2021-09-16 18:57:36 -0400

    Implemented map

---
+++ map.js
@@ -0,0 +1,45 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (Array.isArray(actual[i]) && Array.isArray(expected[i])) {
+        if (!eqArrays(actual[i], expected[i])) {
+          return false;
+        }
+      } else if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  let results = arr1.length === arr2.length;
+  if (results === false) {
+    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      results === true ? (results = arr1[i] === arr2[i]) : (results = false);
+    }
+  }
+  results
+    ? console.log(`🥳🥳🥳 Assertion Passed: [${arr1}] === [${arr2}]`)
+    : console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  return results;
+};
+
+const words = ["pool", "air", "table", "major", "video"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+
+const results1 = map(words, word => word[0]);
+console.log(results1);

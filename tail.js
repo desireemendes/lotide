6039e4598349e2432a07090d481f0b07d1ea995e
Desiree Mendes <desireefrost@live.ca>
2021-09-15 23:18:19 -0400

    tail of array

---
+++ tail.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+function tail(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}

79d94ac9229596f0949213c9bad7d1dde1992321
Desiree Mendes <desireefrost@live.ca>
2021-09-20 13:45:14 -0400

    refactored

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,11 @@
 const assertEqual = function(actual, expected) {
+
   if (actual === expected) {
-    console.log(`🥳 👍 Assertion passed: ${actual} === ${expected}`);
+    console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log('🖐 🛑 Assertion failed: ${actual} !== ${expected}');
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+
 module.exports = assertEqual;
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);

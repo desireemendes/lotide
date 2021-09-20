5fbad7194e743b059812486a2ea251076d2f9fa5
Desiree Mendes <desireefrost@live.ca>
2021-09-20 13:44:54 -0400

    refactored and tested

--- head.js
+++ head.js
@@ -1,13 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🥳 👍 Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log('🖐 🛑 Assertion failed: ${actual} !== ${expected}');
-  }
-};
-
+const assertEqual = require('./assertEqual')
 const head = function(array) {
   return array[0];
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

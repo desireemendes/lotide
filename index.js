1e02d7f9b0c817fa8c090870f80bc081820f7ee6
Desiree Mendes <desireefrost@live.ca>
2021-09-21 00:00:27 -0400

    required functions

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('.assertArraysEqual');
+const eqArrays = require('.eqArrays');
+const assertEqual = require('.assertEqual');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  eqArrays: assertArraysEqual,
+  eqArrays: eqArrays,
+  assertEqual: assertEqual
+
+};
